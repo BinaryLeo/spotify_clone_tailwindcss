693b5b7611c612e83db3b6333620eb2fe835262c
Binaryleo <leomouracss@fastmail.com>
2021-12-17 00:37:33 -0300

    ADD: Extends config. and home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,32 +2,37 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      spacing:{
-        '14':'3.5rem',
-        '125px': '125px'
+      fontFamily: {
+        Montserrat: ["Montserrat"],
       },
-      colors:{
-        'hoverspt': '#18D760',
-        'purple-main':'#2D46B9',
-        'green-main':'#1ED760'
+      spacing: {
+        14: "3.5rem",
+        "125px": "125px",
       },
-      backgroundImage: theme=>({
-        'spotify-theme':"url('/img/bursts.svg')",
-        'spotify-theme-mobile':"url('/img/bursts-mobile.svg')",
-      }),
-      backgroundSize:{
-        '175%':'175%',
-        '195%':'195%',
+      colors: {
+        hoverspt: "#18D760",
+        "purple-main": "#2D46B9",
+        "btn-premium": "#202F72",
+        "green-main": "#1ED760",
+        "green-premium": "#96F0B6",
       },
-      backgroundPosition:{
-        'banner':'46% 4%',
-        'banner-mobile':'top 25% center',
+      backgroundImage: {
+        "spotify-theme": "url('/img/bursts.svg')",
+        "spotify-theme-mobile": "url('/src/img/bursts-mobile.svg')",
       },
-      fontSize:{
-        '9xl': '9rem'
-      }
+      backgroundSize: {
+        "175%": "175%",
+        "195%": "195%",
+      },
+      backgroundPosition: {
+        banner: "46% 4%",
+        "banner-mobile": "top 25% center",
+      },
+      fontSize: {
+        "9xl": "9rem",
+      },
     },
   },
-  variants:{},
+  variants: {},
   plugins: [],
-}
+};
